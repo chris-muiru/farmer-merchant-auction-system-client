e2f6147af4c0c48b2d9bb26745c646c1c3cdd9e9
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-12 16:05:31 +0300

    add: simple shared layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import Layout from "components/Layout"
 import type { AppProps } from "next/app"
+import { NextPage } from "next/types"
+import { ReactElement, ReactNode } from "react"
 import "../styles/globals.css"
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+	getLayout?: (page: ReactElement) => ReactNode
+}
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+	const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>)
+	return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
