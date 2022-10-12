ab6b392c20f29ad17d3a73e35637a9685fe6998a
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-12 16:06:31 +0300

    add: per page layouts

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,60 +1,67 @@
 import Link from "next/link"
+import { ReactElement } from "react"
 import Auth from "../../components/Auth"
 
 const SignUp = () => {
 	return (
 		<Auth>
-			<div className="text-center space-y-1">
-				<h2 className="text-3xl">Hello</h2>
-				<p className="">please enter your details</p>
-			</div>
-			<form action="" className="flex flex-col">
-				<div className="flex flex-col space-y-5">
-					<label htmlFor="email">Email</label>
-					<input
-						type="text"
-						placeholder="Enter your email"
-						className="p-3 rounded-md border focus:border-black focus:outline-none"
-						id="email"
-					/>
-					<label htmlFor="phone">Phone</label>
-					<input
-						type="text"
-						placeholder="Enter your Phone number"
-						className="p-3 rounded-md border focus:border-black focus:outline-none"
-						id="phone"
-					/>
-					<label htmlFor="id">National id</label>
-					<input
-						type="text"
-						className="p-3 rounded-md border focus:border-black focus:outline-none"
-						placeholder="Enter your National id"
-						id="id"
-					/>
-					<label htmlFor="password">Password</label>
-					<input
-						type="password"
-						className="p-3 rounded-md border focus:border-black focus:outline-none"
-						placeholder="Enter your password"
-						id="password"
-					/>
-					<div className="flex flex-row justify-between  text-white">
-						<button className="bg-blue-700 w-60 text-center p-3 rounded-sm">
-							Merchant
-						</button>
-						<button className="bg-green-700 w-60 text-center p-3 rounded-sm">
-							Farmer
-						</button>
-					</div>
+			<>
+				<div className="text-center space-y-1">
+					<h2 className="text-3xl">Hello</h2>
+					<p className="">please enter your details</p>
 				</div>
-				<button
-					type="submit"
-					className="bg-green-700 rounded-sm p-3 mt-16 text-white"
-				>
-					Sign Up
-				</button>
-			</form>
+				<form action="" className="flex flex-col">
+					<div className="flex flex-col space-y-5">
+						<label htmlFor="email">Email</label>
+						<input
+							type="text"
+							placeholder="Enter your email"
+							className="p-3 rounded-md border focus:border-black focus:outline-none"
+							id="email"
+						/>
+						<label htmlFor="phone">Phone</label>
+						<input
+							type="text"
+							placeholder="Enter your Phone number"
+							className="p-3 rounded-md border focus:border-black focus:outline-none"
+							id="phone"
+						/>
+						<label htmlFor="id">National id</label>
+						<input
+							type="text"
+							className="p-3 rounded-md border focus:border-black focus:outline-none"
+							placeholder="Enter your National id"
+							id="id"
+						/>
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							className="p-3 rounded-md border focus:border-black focus:outline-none"
+							placeholder="Enter your password"
+							id="password"
+						/>
+						<div className="flex flex-row justify-between  text-white">
+							<button className="bg-blue-700 w-60 text-center p-3 rounded-sm">
+								Merchant
+							</button>
+							<button className="bg-green-700 w-60 text-center p-3 rounded-sm">
+								Farmer
+							</button>
+						</div>
+					</div>
+					<button
+						type="submit"
+						className="bg-green-700 rounded-sm p-3 mt-16 text-white"
+					>
+						Sign Up
+					</button>
+				</form>
+			</>
 		</Auth>
 	)
 }
+SignUp.getLayout = (page: ReactElement) => {
+	return <>{page}</>
+}
+
 export default SignUp
