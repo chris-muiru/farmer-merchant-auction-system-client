f12d0abe8a293f93514d480b8b4664890c5c6042
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-13 17:18:33 +0300

    update layout

---
+++ components/Home/Layout.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react"
+import Footer from "./Footer"
+import NavBar from "./NavBar"
+interface Iprop {
+	children: JSX.Element
+}
+const Layout: FC<Iprop> = ({ children }) => {
+	return (
+		<>
+			<NavBar />
+			{children}
+		</>
+	)
+}
+
+export default Layout
