c0193e054cd91ee165057dcce81113ff0564a119
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-14 15:49:17 +0300

    add:make navbar fixed and not move with page scroll

--- components/Home/NavBar.tsx
+++ components/Home/NavBar.tsx
@@ -29,8 +29,8 @@ const NavBar = (): JSX.Element => {
 			}
 		}
 	}
-	console.log(categoryDivIsBlock)
 
+	// toggle fiChevron icon between up and down
 	const toggleIcon = () => {
 		if (categoryDivIsBlock) {
 			return <FiChevronUp className="inline" />
@@ -39,81 +39,83 @@ const NavBar = (): JSX.Element => {
 		}
 	}
 	return (
-		<div className="flex flex-col bg-black w-[400px]  text-white min-h-screen">
-			<div className="bg-slate-800">
-				<Image
-					src="/slinger.png"
-					width={150}
-					height={150}
-					className="rounded-md"
-				/>
-			</div>
-			<nav className="space-y-10">
-				<div className="mt-10">
-					Reputation &lt;&lt;&nbsp;
-					<Image src="/star.png" width={12} height={12} />
-					<Image src="/star.png" width={12} height={12} />
-					<Image src="/star.png" width={12} height={12} />
-					<Image src="/star.png" width={12} height={12} />
-					<Image src="/star.png" width={12} height={12} />
-					&nbsp;&gt;&gt;
+		<div className=" bg-black w-[400px]  text-white min-h-screen sticky ">
+			<div className="sticky top-0">
+				<div className="bg-slate-400 ">
+					<Image
+						src="/slinger.png"
+						width={150}
+						height={150}
+						className="rounded-md"
+					/>
 				</div>
-				<div className="hover:text-green-500">
-					<Link href="/">
-						<a>
-							<BsFillHouseFill className="inline mr-2 text-2xl" />
-							Dashboard
-						</a>
-					</Link>
-				</div>
-				<div className="hover:text-green-500">
-					<Link href="/">
-						<a>
-							<FiClipboard className="inline mr-2 text-2xl " />
-							Orders
-						</a>
-					</Link>
-				</div>
-
-				<div>
-					<button
-						onClick={() => {
-							toggleCategory()
-						}}
-						className="mb-3 hover:text-green-500"
-					>
-						<FiMenu className="inline mr-2 text-2xl" />
-						Categories &nbsp;
-						{toggleIcon()}
-					</button>
-
-					<div
-						ref={categoryRef}
-						className=" min-h-[100px] pl-10 space-y-4 box-border hidden"
-					>
-						<p className="hover:text-green-500">hello</p>
-						<p className="hover:text-green-500">hello</p>
-						<p className="hover:text-green-500">hello</p>
-						<p className="hover:text-green-500">hello</p>
+				<nav className="space-y-14 ml-10">
+					<div className="mt-10">
+						Reputation &lt;&lt;&nbsp;
+						<Image src="/star.png" width={12} height={12} />
+						<Image src="/star.png" width={12} height={12} />
+						<Image src="/star.png" width={12} height={12} />
+						<Image src="/star.png" width={12} height={12} />
+						<Image src="/star.png" width={12} height={12} />
+						&nbsp;&gt;&gt;
 					</div>
-				</div>
-				<div className="hover:text-green-500">
-					<Link href="/">
-						<a>
-							<FiMail className="inline mr-2 text-2xl" />
-							Messages
-						</a>
-					</Link>
-				</div>
-				<div className="hover:text-green-500">
-					<Link href="/">
-						<a>
-							<FiSmile className="inline mr-2 text-2xl" />
-							Reviews
-						</a>
-					</Link>
-				</div>
-			</nav>
+					<div className="hover:text-green-500">
+						<Link href="/">
+							<a>
+								<BsFillHouseFill className="inline mr-2 text-2xl" />
+								Dashboard
+							</a>
+						</Link>
+					</div>
+					<div className="hover:text-green-500">
+						<Link href="/">
+							<a>
+								<FiClipboard className="inline mr-2 text-2xl " />
+								Orders
+							</a>
+						</Link>
+					</div>
+
+					<div>
+						<button
+							onClick={() => {
+								toggleCategory()
+							}}
+							className="mb-3 hover:text-green-500"
+						>
+							<FiMenu className="inline mr-2 text-2xl" />
+							Categories &nbsp;
+							{toggleIcon()}
+						</button>
+
+						<div
+							ref={categoryRef}
+							className=" min-h-[100px] pl-10 space-y-4 box-border hidden"
+						>
+							<p className="hover:text-green-500">hello</p>
+							<p className="hover:text-green-500">hello</p>
+							<p className="hover:text-green-500">hello</p>
+							<p className="hover:text-green-500">hello</p>
+						</div>
+					</div>
+					<div className="hover:text-green-500">
+						<Link href="/">
+							<a>
+								<FiMail className="inline mr-2 text-2xl" />
+								Messages
+							</a>
+						</Link>
+					</div>
+					<div className="hover:text-green-500">
+						<Link href="/">
+							<a>
+								<FiSmile className="inline mr-2 text-2xl" />
+								Reviews
+							</a>
+						</Link>
+					</div>
+				</nav>
+			</div>
 		</div>
 	)
 }
