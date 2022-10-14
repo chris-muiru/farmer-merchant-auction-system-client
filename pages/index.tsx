034ca051326c14903c7f44a080e1066b9a69114d
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-14 15:51:44 +0300

    add: header and Product components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+import Header from "components/Header"
+import Product from "components/Home/Product"
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
 import NavBar from "../components/Home/NavBar"
 const Home: NextPage = () => {
-	return <div>hello people</div>
+	return (
+		<div className="min-w-full min-h-screen">
+			<Header />
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+			<Product />
+		</div>
+	)
 }
 
 export default Home
