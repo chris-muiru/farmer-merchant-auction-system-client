6100aabc2f3ccf643e9e7a8502e57d23b7ba2f33
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-18 17:40:46 +0300

    add: tailwind styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 
 const Header = () => {
-	return <div className="text-center text-5xl mb-10 p-4 box-border">Kilimo Mash</div>
+	return (
+		<div className="text-center text-5xl box-border min-h-[157px] flex justify-center items-center">
+			<h2 className="">Kilimo Mash</h2>
+		</div>
+	)
 }
 
 export default Header
