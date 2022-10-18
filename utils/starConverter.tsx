7447a2707150be90ad758a945c95897e58e7d316
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-18 17:45:01 +0300

    add: star converter

---
+++ utils/starConverter.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import { AiFillStar } from "react-icons/ai"
+
+const starConverter = (total: 1 | 2 | 3 | 4 | 5): any[] => {
+	const arr = []
+	for (let i: number = 0; i < total; i++) {
+		arr.push(
+				<AiFillStar className="text-yellow-500 inline" />
+		)
+	}
+	let counter: number = 5 - arr.length
+	for (let i: number = 0; i < counter; i++) {
+		arr.push(<AiFillStar className="text-slate-400 inline" />)
+	}
+	return arr
+}
+export default starConverter
