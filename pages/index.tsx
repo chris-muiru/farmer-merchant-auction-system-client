32f553b85bb8bac46b4cc97b834c5087d5330dae
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-23 23:57:30 +0300

    mv: to Mainfile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
 import Header from "components/Header"
 import Product from "components/Main/Product"
 import type { NextPage } from "next"
+import Main from "components/Main"
 import Head from "next/head"
 import Image from "next/image"
 import NavBar from "../components/NavBar/NavBar"
 const Home: NextPage = () => {
 	return (
-		<div className="min-w-full min-h-screen">
-			<Product />
-			<Product />
-			<Product />
-			<Product />
-			<Product />
+		<div>
+			<Main />
 		</div>
 	)
 }
