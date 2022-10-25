cacf3341c925275aba5b615b92165f4a4ff14d9f
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-25 20:27:34 +0300

    update: tcss

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Header = () => {
 	return (
-		<div className="text-5xl box-border min-h-[157px] flex justify-center items-center">
+		<div className="m-auto text-5xl box-border min-h-[157px] flex justify-center items-center mb-10">
 			<h2 className="">Kilimo Mash</h2>
 		</div>
 	)
