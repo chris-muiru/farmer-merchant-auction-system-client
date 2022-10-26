abe2388526274bcd52c1c7f60d0617a440165c56
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-27 00:39:00 +0300

    add: Orders Link

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link"
 import React from "react"
 import { AiOutlineExclamationCircle } from "react-icons/ai"
 import { MdOutlinePending } from "react-icons/md"
+import { TiTick } from "react-icons/ti"
 const Order = () => {
 	return (
 		<div className="w-[96%] m-auto  p-5 rounded-sm border min-h-screen">
@@ -26,7 +28,11 @@ const Order = () => {
 						<td>22/10/22</td>
 						<td>James Kamau N</td>
 						<td>07555888</td>
-						<td>Two Hp computers</td>
+						<Link href="/orders">
+							<td className="text-blue-800 hover:underline hover:decoration-blue-800 hover:cursor-pointer">
+								Two Hp computers
+							</td>
+						</Link>
 						<td>2</td>
 						<td>24/6/22</td>
 						<td id="declined" className="">
@@ -58,7 +64,7 @@ const Order = () => {
 						<td>2</td>
 						<td>24/6/22</td>
 						<td id="pending" className="text-green-500 mr-1">
-							<MdOutlinePending className="inline" />
+							<TiTick className="inline" />
 							success
 						</td>
 						<td>100000</td>
