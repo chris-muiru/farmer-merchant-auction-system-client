21f40e17962c72bb7e116cb8b02528d856590125
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-27 00:40:45 +0300

    add: Jsx.element type on interface

--- utils/starConverter.tsx
+++ utils/starConverter.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image"
 import { AiFillStar } from "react-icons/ai"
 
-const starConverter = (total: 1 | 2 | 3 | 4 | 5): any[] => {
+const starConverter = (total: 1 | 2 | 3 | 4 | 5): JSX.Element[] => {
 	const arr = []
 	for (let i: number = 0; i < total; i++) {
-		arr.push(
-				<AiFillStar className="text-yellow-500 inline" />
-		)
+		arr.push(<AiFillStar className="text-yellow-500 inline" />)
 	}
 	let counter: number = 5 - arr.length
 	for (let i: number = 0; i < counter; i++) {
