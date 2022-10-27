cd9e25b3302a1f57b8e6314b0dd60de63976b219
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-27 10:51:26 +0300

    add: selection stars component

---
+++ components/Reviews/SelectionStars.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { AiFillStar } from "react-icons/ai"
+
+const SelectionStars = () => {
+	return (
+		<div className="w-3/4 m-auto  text-4xl mb-5 gap-4 flex justify-center">
+			<AiFillStar className="inline" />
+			<AiFillStar className="inline" />
+			<AiFillStar className=" inline" />
+			<AiFillStar className=" inline" />
+			<AiFillStar className=" inline" />
+		</div>
+	)
+}
+export default SelectionStars
