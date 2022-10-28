671c77d8c5db8abf54838b4d3061e431f0b8735e
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-28 09:44:11 +0300

    add: createProduct component

---
+++ components/NavBar/CreateProduct.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { BsPlusLg } from "react-icons/bs"
+const CreateProduct = () => {
+	return (
+		<button className="bg-blue-500 w-[50px] text-center h-[50px] flex items-center justify-center">
+			Sell
+			{/* <BsPlusLg className="text-3xl text-white" /> */}
+		</button>
+	)
+}
+
+export default CreateProduct
