a7819232d9b1da2915a958055bf448ea8b47abbf
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-28 13:59:34 +0300

    modify: remove unused function

--- components/NavBar/Reputation.tsx
+++ components/NavBar/Reputation.tsx
@@ -1,16 +1,11 @@
 import React from "react"
 import Image from "next/image"
 import starConverter from "utils/starConverter"
-const count = 5
+const count = 2
 interface Iprop {
 	count?: number
 }
-const stars = (counter: number) => {
-	while (true) {
-		if (counter < 5) {
-		}
-	}
-}
+
 const Reputation = () => {
 	return (
 		<div className="mt-10">
