9756a55ffec8131d45911fac1966b22f8d12996d
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-28 14:03:31 +0300

    modify: upate tcss

--- utils/starConverter.tsx
+++ utils/starConverter.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image"
 import { AiFillStar } from "react-icons/ai"
 
-const starConverter = (total: 1 | 2 | 3 | 4 | 5): JSX.Element[] => {
+const starConverter = (total: 0 | 1 | 2 | 3 | 4 | 5): JSX.Element[] => {
 	const arr = []
 	for (let i: number = 0; i < total; i++) {
 		arr.push(<AiFillStar className="text-yellow-500 inline" />)
 	}
 	let counter: number = 5 - arr.length
 	for (let i: number = 0; i < counter; i++) {
-		arr.push(<AiFillStar className="text-slate-400 inline" />)
+		arr.push(<AiFillStar className="inline text-slate-200" />)
 	}
 	return arr
 }
