d3e6142414f49c326f5964b7ca9850f21a8b60fd
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-28 09:46:48 +0300

    rm: unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
-import Header from "components/Header"
-import Product from "components/Main/Product"
 import type { NextPage } from "next"
 import Main from "components/Main"
-import Head from "next/head"
-import Image from "next/image"
-import NavBar from "../components/NavBar/NavBar"
 const Home: NextPage = () => {
 	return (
-		<div>
+		<div className="m-auto">
 			<Main />
 		</div>
 	)
