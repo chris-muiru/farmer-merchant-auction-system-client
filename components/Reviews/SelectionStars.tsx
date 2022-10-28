570e9be5373504ead95673bb7208aa17a9ae3c76
kris-slinger <krisdjangodeveloper@gmail.com>
2022-10-28 09:46:10 +0300

    add: review stars

--- components/Reviews/SelectionStars.tsx
+++ components/Reviews/SelectionStars.tsx
@@ -2,14 +2,17 @@ import React from "react"
 import { AiFillStar } from "react-icons/ai"
 
 const SelectionStars = () => {
+	const stars: JSX.Element[] = [
+		<AiFillStar className="inline" />,
+		<AiFillStar className="inline" />,
+		<AiFillStar className=" inline" />,
+		<AiFillStar className=" inline" />,
+		<AiFillStar className=" inline" />,
+	]
 	return (
 		<div className="w-3/4 m-auto  text-4xl mb-5 gap-4 flex justify-center">
-			<AiFillStar className="inline" />
-			<AiFillStar className="inline" />
-			<AiFillStar className=" inline" />
-			<AiFillStar className=" inline" />
-			<AiFillStar className=" inline" />
-		</div>
+            {stars}
+        </div>
 	)
 }
 export default SelectionStars
