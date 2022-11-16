1f67e5b365953d519b7ffa054a5d9530dc051af1
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-16 04:47:37 +0300

    add: logout functionality

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,9 +1,15 @@
+import { useAuthContext } from "context/AuthContextProvider"
 import React from "react"
+import { MdLogout } from "react-icons/md"
 
 const Header = () => {
+	const { logOut } = useAuthContext()
 	return (
-		<div className="m-auto text-5xl box-border min-h-[157px] flex justify-center items-center mb-10">
+		<div className="m-auto text-5xl box-border min-h-[157px] flex justify-center items-center mb-10 w-3/4 relative">
 			<h2 className="">Kilimo Mash</h2>
+			<button className="hover:text-green-500" onClick={logOut}>
+				<MdLogout className="absolute right-20 top-12" />
+			</button>
 		</div>
 	)
 }
