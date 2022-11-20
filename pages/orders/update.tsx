ee4d59bd5d9d7b6bef6d64685bef44815232fefe
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-20 23:16:21 +0300

    add: order update functionality

---
+++ pages/orders/update.tsx
@@ -0,0 +1,12 @@
+import OrderStatusModal from "components/Order/OrderStatusModal"
+import React from "react"
+
+const OrderUpdate = () => {
+	return (
+		<div>
+			<OrderStatusModal />
+		</div>
+	)
+}
+
+export default OrderUpdate
