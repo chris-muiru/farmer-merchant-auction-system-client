b281dbfba10e7467486cf201ef0912e3f2284c5e
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-20 23:15:50 +0300

    update: add rgba color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			backgroundColor: {
+				modal: "rgb(226 232 240,0.7)",
+			},
+		},
+	},
+	plugins: [],
 }
