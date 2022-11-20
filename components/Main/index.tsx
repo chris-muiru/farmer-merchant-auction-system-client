06409d550f557f22254acf03b5dcfec87f728110
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-20 23:08:55 +0300

    update: display header based on role

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,14 +1,15 @@
 import ProductGroup from "components/Product/ProductGroup"
+import { useAuthContext } from "context/AuthContextProvider"
 import React from "react"
 import Product from "../Product/Product"
 
 const Main = () => {
+	const { role } = useAuthContext()
 	return (
 		<div className="min-h-screen flex flex-col w-[90%] m-auto p-10 space-y-3">
-			<h2 className="text-2xl p-2">{"My products"}</h2>
+			{role == "merchant" && <h2 className="text-2xl p-2">{"Products"}</h2>}
+			{role == "farmer" && <h2 className="text-2xl p-2">{"My Products"}</h2>}
 			<ProductGroup />
-			{/* <Product /> */}
-			{/* <Product /> */}
 		</div>
 	)
 }
