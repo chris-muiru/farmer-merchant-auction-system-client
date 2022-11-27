58a2fb90e8ff716024c2e5f08863c6a5512755ee
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-27 20:42:59 +0300

    update: update width and mergin

--- components/Auth/Auth.tsx
+++ components/Auth/Auth.tsx
@@ -8,11 +8,11 @@ const Auth: NextPage<Props> = ({ children }) => {
 	return (
 		<div>
 			<div className="flex flex-row min-h-screen">
-				<div className="w-full">
-					<div className="m-auto w-1/2 mt-20 space-y-10 ">{children}</div>
+				<div className="w-3/4">
+					<div className="m-auto w-1/2 mt-10 space-y-10 ">{children}</div>
 				</div>
 				<div className="w-full relative min-h-screen">
-					<Image src="/fruits.jpg" layout="fill" />
+					<Image src="/login.png" layout="fill" />
 				</div>
 			</div>
 		</div>
