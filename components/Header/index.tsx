501d238f545d6c27706a9ed5de17f848cf852880
kris-slinger <krisdjangodeveloper@gmail.com>
2022-11-29 19:14:02 +0300

    update: tailwindcss styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,10 +5,13 @@ import { MdLogout } from "react-icons/md"
 const Header = () => {
 	const { logOut } = useAuthContext()
 	return (
-		<div className="m-auto text-5xl box-border min-h-[157px] flex justify-center items-center mb-10 w-3/4 relative">
-			<h2 className="">Kilimo Mash</h2>
-			<button className="hover:text-green-500" onClick={logOut}>
-				<MdLogout className="absolute right-20 top-12" />
+		<div className="m-auto box-border   min-h-[157px] flex justify-center items-center mb-10 w-full relative">
+			<h2 className="text-3xl">Kilimo Mash</h2>
+			<button
+				className="hover:text-green-500 text-2xl absolute right-[200px] bottom-15 border-4 p-1 rounded-md w-20 flex justify-center items-center"
+				onClick={logOut}
+			>
+				<MdLogout />
 			</button>
 		</div>
 	)
