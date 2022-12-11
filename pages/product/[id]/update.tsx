a6995729f7572c6a261b797bfd829d16abf1d051
kris-slinger <krisdjangodeveloper@gmail.com>
2022-12-11 22:42:16 +0300

    update: accept certain file types

--- pages/product/[id]/update.tsx
+++ pages/product/[id]/update.tsx
@@ -176,6 +176,7 @@ const Update = () => {
 								onChange={setImageData}
 								type="file"
 								// multiple={true}
+								accept=".png,.jpeg,.jpg"
 								className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
 							/>
 						</label>
