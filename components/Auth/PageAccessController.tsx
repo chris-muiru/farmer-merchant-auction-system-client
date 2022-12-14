620369d712ca11516c576235a976532e8e4e9b42
kris-slinger <krisdjangodeveloper@gmail.com>
2022-12-14 09:46:16 +0300

    initial commit

---
+++ components/Auth/PageAccessController.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+// TODO: handle page access using this controller
+const PageAccessController = () => {
+	return <div>PageController</div>
+}
+
+export default PageAccessController
