8bb088169f68cccffe2e7b7227d3371ddbd41b85
kris-slinger <krisdjangodeveloper@gmail.com>
2022-12-17 10:05:59 +0300

    add: Logo

---
+++ components/Logo/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Image from "next/image"
+const Logo = () => {
+	return (
+		<Image
+			alt="add slinger photo"
+			src="/logo-with-name.svg"
+			width={250}
+			height={250}
+		/>
+	)
+}
+
+export default Logo
